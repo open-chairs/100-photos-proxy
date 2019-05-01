d1bd010ed0d32c79b68449b36fb14b6f0bf4f257
Ray Lin <raylin009@gmail.com>
2019-04-30 18:57:59 -0700

    removed other modules

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use('/:id', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use('/image/*', (req, res) => {
+//   axios[req.method.toLowerCase()](`http://localhost:3002/${req.originalUrl}`)
+//   .then(({data}) => res.send(data))
+//   .catch(err => res.statusCode(500).send(err))
+// });
 
+app.use('/:id', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
